a90ce95656fcffdc975959b15126a3d84e57056e
KonsDouk <konstdoukas@disroot.org>
2023-06-23 16:58:45 +0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
+import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap-vue/dist/bootstrap-vue.css";
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { BootstrapVue } from "bootstrap-vue";
+// import "./assets/main.css";
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+
+// Make BootstrapVue available throughout your project
+createApp.use(BootstrapVue);
+
+// // Optionally install the BootstrapVue icon components plugin
+// createApp.use(IconsPlugin);
+
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/js/bootstrap.js'
 
 createApp(App).use(store).use(router).mount('#app')
