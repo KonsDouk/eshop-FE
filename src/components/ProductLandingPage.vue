4cb6cf7bc91467126e8a0a265ce415ea3bbcc5a2
KonsDouk <konstdoukas@disroot.org>
2023-06-24 17:35:22 +0300

    save

--- src/components/ProductLandingPage.vue
+++ src/components/ProductLandingPage.vue
@@ -1,20 +1,28 @@
 <template>
     <div><!-- Το div πρέπει να τα περιέχει όλα -->
         <div class="product-title">
-            <h3>Product Title Goes Here</h3>
+            <h3>{{ this.productObject.productName }}</h3>
         </div>
 
         <div class="product-order-container">
-            <div class="product-img">
-                <img class="img" src="../../public/images/3jO7N1x.jpg" alt="test product">
+            <div>
+                <div class="product-img">
+                    <img class="img" :src="require(`../../public/images/${this.showSelectedImage}.jpg`)" alt="test product">
+                </div>
+                <div class="more-imgs">
+                    <img class="smaller-imgs" src="../../public/images/img1.jpg" alt="img1" @click="changeMainImage">
+                    <img class="smaller-imgs" src="../../public/images/img2.jpg" alt="img2" @click="changeMainImage">
+                    <img class="smaller-imgs" src="../../public/images/img3.jpg" alt="img3" @click="changeMainImage">
+                    <img class="smaller-imgs" src="../../public/images/img4.jpg" alt="img4" @click="changeMainImage">
+                </div>
             </div>
 
             <div class="order-btns">
                 <div class="price">
-                    <p>10000 €</p>
+                    <p>{{  this.productObject.productPrice}} €</p>
                 </div>
                 <div class="product-availability">
-                    <p>Διαθεσιμότητα: Άμεσα διαθέσιμο</p>
+                    <p>Διαθεσιμότητα: {{ (this.productObject.availableQty > 0) ? 'Άμεσα διαθέσιμο' : 'Κατόπιν παραγγελίας' }}</p>
                 </div>
 
                 <div class="quantity-controls">
@@ -32,21 +40,39 @@
                     <button class="add-to-cart">Προσθήκη στο καλάθι</button>
                 </div>
             </div>
+        </div>
 
+        <div class="product-description">
+            <span class="tabs" @click="showDescription"><button>Περιγραφή</button></span>
+            <span class="tabs" @click="showCharacteristics"><button>Χαρακτηριστικά</button></span>
+            <div class="product-description-text" v-if="this.descriptionFlag">
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error a quaerat facilis numquam temporibus vitae molestias assumenda quis natus, animi, modi autem ut nulla expedita hic voluptas nam, odio reprehenderit consequatur earum quas tenetur nostrum necessitatibus sequi. Nesciunt temporibus distinctio soluta nam est placeat ducimus! Animi repellat velit harum dolorum?</p>
+            </div>
+            <div class="characteristics" v-if="this.characteristicsFlag">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime, distinctio!</p>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 export default {
+    props: {
+        productObject: {
+            required: true,
+            type: Object
+        }
+    },
     data(){
         return {
             productQuantity: 5,
-            
+            showSelectedImage: 'img1',
+            descriptionFlag : true,
+            characteristicsFlag: false,
         }
     },
     mounted(){
-        
+        console.log(this.productObject)
     },
     methods: {
         increaseProductQuantity(){
@@ -55,14 +81,27 @@ export default {
         decreaseProductQuantity(){
             this.productQuantity--
         },
-        checkForNegative(){
-            if (this.productQuantity < 0){
-                this.productQuantity = 1
-            }
+        changeMainImage(x){
+            //Πατέντα! Να παίρνει το όνομα από την βάση και με εκείνο να επιλέγει την εικόνα που θα φαίνεται
+            this.showSelectedImage = x.target.alt
+        },
+        showDescription(){
+            this.characteristicsFlag = false
+            this.descriptionFlag = true
+        },
+        showCharacteristics(){
+            this.descriptionFlag = false
+            this.characteristicsFlag = true
         }
         
     },
     watch: {
+        productQuantity(nv){
+            if (nv < 0){
+                this.productQuantity = 1
+            }
+        },
+
     }
 }
 </script>
@@ -153,5 +192,30 @@ input[type=number] {
 }
 .img:hover {
     transform: scale(1.2);
+}
+
+.smaller-imgs {
+    margin: 1em;
+    max-width: 4.5em;
+    max-height: 5em;
+}
+.smaller-imgs:hover {
+    cursor: pointer;
+}
+
+
+.tabs > button {
+    background: none;
+    border:none;
+    margin: 1em;
+    border-bottom: solid 2px black;
+}
+.tabs > button:hover {
+    transform: scale(1.2);
+}
+
+.product-description {
+    max-width: 70%;
+    margin: auto;
 }
 </style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,36 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div>
+      <ProductLandingPage
+      :productObject="product"
+      />
+    </div>
 </template>
+
+<script>
+import ProductLandingPage from '@/components/ProductLandingPage.vue';
+export default {
+  components: {
+    ProductLandingPage,
+
+  },
+  data(){
+    return {
+      product: {
+        productId: 1,
+        productName: 'Alienware XTreme Hardcore Tryhard Terminator 9000',
+        productPrice: 15000,
+        availableQty: 100,
+        productDescription: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error a quaerat facilis numquam temporibus vitae molestias assumenda quis natus, animi, modi autem ut nulla expedita hic voluptas nam, odio reprehenderit consequatur earum quas tenetur nostrum necessitatibus sequi. Nesciunt temporibus distinctio soluta nam est placeat ducimus! Animi repellat velit harum dolorum?',
+        productCharacteristics: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime, distinctio!',
+      }
+    }
+  },
+  async mounted(){
+
+  }
+}
+</script>
+
+<style>
+
+</style>
